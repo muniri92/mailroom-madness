5fc2e52b230287f1c90df992298cd4157bdf7d16
muniri92 <MunirIbrahim@Munirs-MacBook-Pro.local>
2016-03-03 15:50:15 -0800

    got a working control flow for initial function

--- mailroom.py
+++ mailroom.py
@@ -1,31 +1,35 @@
 # _*_ coding: utf-8 _*_
 
+# try:
+# 	input = raw_input
+# except NameError:
+# 	pass
+
 
 donor = {"Tim": "200", "Jim": "10.90"}
 
-start_input = """Hello! Press 'Q' to quit.
-Would you like to Send a Thank You(1), or Create a Report(2)?\n
-"""
-start = input(start_input)
 
-
-def mailroom(starting):
-    if starting == 1:
+def mailroom():
+    start_input = """Hello! Press 'Q' to quit.
+    Would you like to Send a Thank You(1), or Create a Report(2)?\n
+    """
+    start = input(start_input)
+    while start not in ("1", "2"):
+        start = input(start_input)
+        continue
+    if start == "1":
         thank_you()
-    elif starting == 2:
-        send_report()
     else:
-        while starting not in ["1", "2"]:
-            start = input(start_input)
+        send_report()
 
 
 def thank_you():
-    print("thanks")
+    print(u"thanks")
 
 
 def send_report():
-    print("report")
+    print(u"report")
 
 
-mailroom(start)
+mailroom()
 # thank_you()
