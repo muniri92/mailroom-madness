9314f02b1e9e0cc972459b9c273c269733109358
Michael Sullivan <sapientia.veritas@gmail.com>
2016-03-04 13:17:03 -0800

    added skeleton tests to test_mailroom for expansion

--- test_mailroom.py
+++ test_mailroom.py
@@ -1,8 +1,27 @@
 # _*_ coding: utf-8 _*_
+import pytest
 
 
 donor = {"Tim": "200", "Jim": "10.90"}
 
 
+@pytest.mark.parametrize()
 def test_mailroom():
-    input("value")
+    from mailroom import mailroom
+    asert [[blank]]] == result
+
+
+@pytest.mark.parametrize()
+def test_send_letter():
+    from mailroom import send_letter
+    assert [[blank]] == result
+
+@pytest.mark.parametrize()
+def test_add_donor():
+    from mailroom import add_donor
+    assert 1 == result
+
+@pytest.mark.parametrize()
+def test_send_report():
+    from mailroom import send_report
+    assert 1 == result
